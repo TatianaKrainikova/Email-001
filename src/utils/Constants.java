7d694446dc1cf05aedcf0cd0564ba8b6318b5d05
Tanya <krajnikova2004@gmail.com>
2023-08-07 18:37:55 +0100

    Create input email validation

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     public static String NAN_MSG = "\"%s\" - not a number!%nTry again: ";
     public final static String NO_SUCH_OPTION_MSG = "There is no such option! Try again: ";
     public final static String NAME_EMPTY_MSG = "Empty! Please enter a name: ";
-    public final static String PHONE_EMPTY_MSG = "Empty! Enter phone: ";
+    public final static String EMAIL_EMPTY_MSG = "Empty! Enter email: ";
 }
---
+++ src/utils/EmailValidation.java
@@ -0,0 +1,15 @@
+package utils;
+
+import java.util.Scanner;
+
+public class EmailValidation {
+
+    public static String validateEmailInput(Scanner scanner) {
+        String str = scanner.nextLine().trim();
+        while (str.isEmpty()) {
+            System.out.println(Constants.EMAIL_EMPTY_MSG);
+            str = scanner.nextLine().trim();
+        }
+        return str;
+    }
+}
